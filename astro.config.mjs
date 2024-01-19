eb944801acd72ed3335868f913cc5fc556d8d39d
Raúl Abad <raulabadtorralba@gmail.com>
2024-01-20 00:15:23 +0100

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
 
   site: 'https://portfolio.github.io',
-  base: '/portfolio',
+  base: '/portfolio/about',
   server: { open: "portfolio/about" },
  
 });
